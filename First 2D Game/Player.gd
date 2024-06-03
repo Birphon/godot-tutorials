ce4d21a900a3bbefce8da1a599c668f01993bbe2
Birphon <61985658+Birphon@users.noreply.github.com>
2024-06-04 00:35:47 +1200

    reddit help changes

--- First 2D Game/Player.gd
+++ First 2D Game/Player.gd
@@ -19,12 +19,18 @@ func _process(delta):
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
 	
-	if Input.is_action_pressed("move_up"):
+	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
 	
-	if Input.is_action_pressed("move_down"):
+	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
-	
+		
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+		
 	# Staying within the screen
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
